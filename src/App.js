fdd80110542a54534fa43353c6fdc39388f7e6f7
Sanjay Kumar <sanjaytummala18@gmail.com>
2025-12-01 15:56:15 +0530

    Connected frontend to deployed backend

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const detect = async () => {
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch(process.env.REACT_APP_API_URL + "/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
